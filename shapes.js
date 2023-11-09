a441d34a106f3aa8068e9e9b9712c28fda228ffe
Sshiril <113382540+Sshiril@users.noreply.github.com>
2023-11-09 15:22:35 -0500

    Create shapes.js

---
+++ shapes.js
@@ -0,0 +1,19 @@
+function setup(){
+createCanvas(windowWidth,windowHeight);
+  //background(255,165,0);
+  strokeWeight(6);
+ // rect(130,90,260,120);
+//fill(255,165,0);
+  // strokeWeight(1);
+ // ellipse(370,190,140,200);
+ /* strokeWeight(8);
+  stroke(0,0,255);
+  point(250,250);
+  line(60,50,37,240);*/
+  //ellipse(mouseX,mouseY,50,50);
+  
+}
+function draw(){
+   ellipse(mouseX,mouseY,50,50);
+  
+}
